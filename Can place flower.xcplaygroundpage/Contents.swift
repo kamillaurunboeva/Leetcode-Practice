7eba3a9a642e93dc18f81046db2ab6d13a3d8575
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-05-19 23:20:03 -0400

    Update Contents.swift

--- Can place flower.xcplaygroundpage/Contents.swift
+++ Can place flower.xcplaygroundpage/Contents.swift
@@ -20,8 +20,20 @@ Constraints:
 flowerbed[i] is 0 or 1.
 There are no two adjacent flowers in flowerbed.
 0 <= n <= flowerbed.length*/
-import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+class Solution {
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        var flowerbed = flowerbed
+        var placed = 0
+        
+        for (idx, plot) in flowerbed.enumerated() where plot != 1 {
+            let prevIdx = (idx - 1), nextIdx = (idx + 1)
+            if prevIdx >= 0, flowerbed[prevIdx] != 0 { continue }
+            if nextIdx < flowerbed.count, flowerbed[nextIdx] != 0 { continue }
+            flowerbed[idx] = 1
+            placed += 1
+        }
+        return placed >= n
+    }
+}
