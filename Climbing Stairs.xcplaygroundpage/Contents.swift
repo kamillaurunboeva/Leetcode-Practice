b80a86abc98ba703735a9c4dfc9ca1f1d959c5c9
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-06-03 23:53:21 -0400

    Update Contents.swift

--- Climbing Stairs.xcplaygroundpage/Contents.swift
+++ Climbing Stairs.xcplaygroundpage/Contents.swift
@@ -28,3 +28,20 @@ import Foundation
  Constraints:
 
  1 <= n <= 45*/
+
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+       if n <= 2{ 
+        return n
+       }
+        var first = 1
+        var second = 2
+
+        for i in 3...n {
+            let third = first + second
+            first = second
+            second = third 
+        }
+            return second
+    }
+}
